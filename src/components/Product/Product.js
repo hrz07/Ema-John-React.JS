171906a6c8cc5a9b2294eb7b54288d8ff6b5761e
hrz07 <hridoy.developer@yahoo.com>
2022-03-22 21:40:16 +0600

    cart design complete

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './Product.css'
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
 
     return (
         <div className='productCart'>
             <img src={product.img} alt="" srcset="" />
-            <p>{product.name}</p>
-            <p>${product.price}</p>
-            <small>Manufacturer: {product.seller}</small> <br />
-            <small>Rating: {product.ratings}</small>
+            <div className='productDetails'>
+                <p className='productName'>{product.name}</p>
+                <p>${product.price}</p>
+                <small>Manufacturer: {product.seller}</small> <br />
+                <small>Rating: {product.ratings}</small>
+            </div>
+           
+                <button className="cartFooter">Add To Cart</button>
             
+
         </div>
     );
 }
